e97f055d14e722b488a85eab6bf9ff5cbf7707b8
mkmelhundley-code <mkmelhundley@gmail.com>
2025-11-20 09:15:48 -0800

    created a synonym component and sent the synonym data to loop through the synonyms if they are present.

--- src/Meaning.jsx
+++ src/Meaning.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       <p>
+        <strong>Definition: </strong>
         {props.meaning.definition}
         <br />
-        <em>{props.meaning.example}</em>
+        <em>
+          <strong>Example: </strong>
+          {props.meaning.example}
+        </em>
+        <br />
+        <Synonyms synonyms={props.meaning.synonyms} />
       </p>
     </div>
   );

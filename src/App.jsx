98404774024a56fe76ba5284c9cb3ee5a4887af1
mkmelhundley-code <mkmelhundley@gmail.com>
2025-12-07 13:04:55 -0800

    Updated logo and checked for node.js vulnerabilities

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import logo from "./MJH-ds-logo.png";
+import logo from "./MJHds-logo.png";
 
 import Dictionary from "./Dictionary.jsx";
 import Footer from "./Footer.jsx";
